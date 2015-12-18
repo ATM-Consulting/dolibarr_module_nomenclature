89b2d7a22d889ace13b39fe1ce698ad2ace1a28c
Alexis Algoud <alexis@atm-consulting.fr>
2015-12-18 10:58:11 +0100

    Improve speed mode

--- js/speed.js
+++ js/speed.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-	//http://camohub.github.io/jquery-sortable-lists/
-	var options = {
+var options = {
 		placeholderClass: 'placeholderClass',
 		hintClass: 'hintClass',
 		ignoreClass: 'clicable',
@@ -22,12 +20,44 @@ $(document).ready(function() {
 			}
 		},
 		onChange:function(cEl) {
-			console.log( $('#speednomenclature').sortableListsToArray());
+		  console.log( $('#speednomenclature').sortableListsToArray());
+		  console.log( $('#speednomenclature').sortableListsToHierarchy());
 		  $('div.logme').html( );	
 		}
 	};
 
+$(document).ready(function() {
+	//http://camohub.github.io/jquery-sortable-lists/
 	$('#speednomenclature a').addClass('clicable');
 	$('#speednomenclature').sortableLists( options );
 	
+	
+	$('#speednomenclature').mousemove(function (e) {
+        $('div#addto').offset({ top: e.pageY - 50 });
+    });
+	
+	$('#speednomenclature li').mouseenter(function(e) {
+		$('#speednomenclature li').removeClass('selectedElement');
+		$(this).addClass('selectedElement');
+	});
+	
+	$('input[name=AddProductNomenclature]').click(function() {
+		addProduct($('select#fk_product').val(),$('select#fk_product option:selected').text());
+	});
+	
 });
+
+function addProduct(fk_product,label) {
+	console.log('addProduct',fk_product,label);
+	if($('li.selectedElement>ul').length == 0)$('li.selectedElement').append('<ul />');
+	$to = $('li.selectedElement>ul');
+	
+	if(label == '')label='...';
+	$li = $('<li id="'+fk_product+'" line-type="product" class="newElement">'+label+'</li>');
+	$li.data('value', {
+		fk_product:fk_product
+	});
+	
+	$to.append($li);
+	
+}
--- nomenclature-speed.php
+++ nomenclature-speed.php
@@ -3,6 +3,8 @@
 	require 'config.php';
 	dol_include_once('/nomenclature/class/nomenclature.class.php');
 	
+	$langs->load('nomenclature@nomenclature');
+	
 	$object_type = GETPOST('object');	
 	$id = (int)GETPOST('id');
 	
@@ -20,9 +22,19 @@
 	
 	
 function _drawlines(&$object) {
-	global $db,$langs,$conf;
+	global $db,$langs,$conf,$PDOdb;
 	
 	llxHeader('', 'Nomenclatures', '', '', 0, 0, array('/nomenclature/js/speed.js','/nomenclature/js/jquery-sortable-lists.min.js'), array('/nomenclature/css/speed.css'));
+	
+	$formDoli=new Form($db);
+	$formCore=new TFormCore;
+	echo '<div id="addto" style="float:right; width:200px;">';
+		$formDoli->select_produits(-1,'fk_product');
+		echo $formCore->bt($langs->trans('AddProductNomenclature'), 'AddProductNomenclature');
+		
+		echo $formCore->combo('', 'fk_new_workstation',TWorkstation::getWorstations($PDOdb, false, true), -1);
+		echo $formCore->bt($langs->trans('AddWorkstation'), 'AddWorkstation');
+	echo '</div>';
 	
 	echo '<ul id="speednomenclature" class="lines '.$object->element.'">';
 	
@@ -33,7 +45,9 @@ function _drawlines(&$object) {
 			$product = new Product($db);
 			$product->fetch($line->fk_product);
 			
-			echo '<li class="product" line-type="product" id="'.$line->fk_product.'"><div>'.$product->getNomUrl(1).' '.$product->label.'</div>';
+			echo '<li class="product" line-type="product" id="'.$line->fk_product.'"><div>'.$product->getNomUrl(1).' '.$product->label;
+			
+			echo '</div>';
 			
 			_drawnomenclature($line->id, 'propal',$line->fk_product,$line->qty);
 			
@@ -56,15 +70,20 @@ function _drawnomenclature($fk_object, $object_type,$fk_product,$qty) {
 	$nomenclature=new TNomenclature;
 	$nomenclature->loadByObjectId($PDOdb, $fk_object, $object_type, true,$fk_product,$qty);
 	
-	if(!empty($nomenclature->TNomenclatureDet)) {
+	if(!empty($nomenclature->TNomenclatureDet) || !empty($nomenclature->TNomenclatureWorkstation)) {
 		echo '<ul class="lines nomenclature" id="'.$nomenclature->getId().'">';
 		
-		foreach($nomenclature->TNomenclatureDet as $line) {
+		foreach($nomenclature->TNomenclatureDet as &$line) {
 			$product = new Product($db);
 			$product->fetch($line->fk_product);
-			echo '<li class="nomenclature" line-type="nomenclature" id="'.$line->getId().'"><div>'.$product->getNomUrl(1).' '.$product->label.'</div></li>';
-				
-			
+			echo '<li class="nomenclature" line-type="nomenclature" id="'.$line->getId().'"><div>'.$product->getNomUrl(1).' '.$product->label.'</div>';
+				_drawnomenclature($product->id, 'product',$product->id,$line->qty * $qty);		
+			echo '</li>';
+		}
+		
+		foreach($nomenclature->TNomenclatureWorkstation as &$ws) {
+			echo '<li class="nomenclature workstation" line-type="workstation" id="'.$ws->workstation->getId().'"><div>'.$ws->workstation->name.'</div>';
+			echo '</li>';
 		}
 		
 		echo '</ul>';
