45f4bc92122a5d56ebecc1b15472fa3552746365
phf <phf@atm-consulting.fr>
2017-04-12 15:09:57 +0200

    Fix trad missing

--- core/modules/modnomenclature.class.php
+++ core/modules/modnomenclature.class.php
@@ -191,20 +191,20 @@ class modnomenclature extends DolibarrModules
 		// Add here list of permission defined by an id, a label, a boolean and two constant strings.
 		// Example:
         $this->rights[$r][0] = $this->numero + $r;  // Permission id (must not be already used)
-        $this->rights[$r][1] = 'Read';  // Permission label
+        $this->rights[$r][1] = 'nomenclatureRead';  // Permission label
         $this->rights[$r][3] = 1;                   // Permission by default for new user (0/1)
         $this->rights[$r][4] = 'read';              // In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
         $r++;
 
         $this->rights[$r][0] = $this->numero + $r;  // Permission id (must not be already used)
-        $this->rights[$r][1] = 'Write';  // Permission label
+        $this->rights[$r][1] = 'nomenclatureWrite';  // Permission label
         $this->rights[$r][3] = 0;                   // Permission by default for new user (0/1)
         $this->rights[$r][4] = 'write';              // In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
         $r++;
 
 
         $this->rights[$r][0] = $this->numero + $r;  // Permission id (must not be already used)
-        $this->rights[$r][1] = 'ShowPrice';  // Permission label
+        $this->rights[$r][1] = 'nomenclatureShowPrice';  // Permission label
         $this->rights[$r][3] = 0;                   // Permission by default for new user (0/1)
         $this->rights[$r][4] = 'showPrice';              // In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
         $r++;
