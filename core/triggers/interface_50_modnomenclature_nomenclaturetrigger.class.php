721a94cb7d94438eb549a5fef08a6bc30ef8b39d
root <root@vps67041.vps.ovh.ca>
2017-11-27 15:21:41 +0100

    round value

--- core/triggers/interface_50_modnomenclature_nomenclaturetrigger.class.php
+++ core/triggers/interface_50_modnomenclature_nomenclaturetrigger.class.php
@@ -280,8 +280,9 @@ class Interfacenomenclaturetrigger
 			$sell_price_to_use=$n->totalPV;
 		}
 
-
 		if(empty($sell_price_to_use)) return 0;
+
+		$sell_price_to_use = price2num($sell_price_to_use,'MT'); //round value
 
 		if($object_type=='commande') {
 //		var_dump($n->totalPV, $object_type,$object);exit;
