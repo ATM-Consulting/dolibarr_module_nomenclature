93f04d6e8df39f4525ec0a2b7588016f726771f2
alexis Algoud <alexis@atm-consulting.fr>
2018-03-20 10:28:58 +0100

    info not imported

--- admin/import.php
+++ admin/import.php
@@ -2,27 +2,29 @@
 
 	require '../config.php';
 
+	ini_set('memory_limit','1024M');
+
 	dol_include_once('/core/lib/admin.lib.php');
 	dol_include_once('/nomenclature/lib/nomenclature.lib.php');
 	dol_include_once('/nomenclature/class/nomenclature.class.php');
-	
+
 	dol_include_once('/product/class/product.class.php');
-	
+
 	$langs->load("nomenclature@nomenclature");
 	$PDOdb = new TPDOdb;
-			
+
 	// Access control
 	if (! $user->admin) {
 	    accessforbidden();
 	}
-	
+
 	_card($PDOdb);
-	
+
 function _card(&$PDOdb) {
 	global $langs, $user;
-	
+
 	llxHeader();
-	
+
 	$head = nomenclatureAdminPrepareHead();
 	dol_fiche_head(
 	    $head,
@@ -30,38 +32,43 @@ function _card(&$PDOdb) {
 	    $langs->trans("Module104580Name"),
 	    0,
 	    "nomenclature@nomenclature"
-	);	
-	
+	);
+
 	$formCore=new TFormCore('auto','formImport','post',true);
-	
+
 	echo $formCore->fichier('Fichier source'.img_help(1, 'Numéro nomenclature,Ref produit, Ref Composant, Qté, Qté de référence, Code type (MO pour Poste de travail)') , 'file1', '', 40);
 	echo $formCore->btsubmit('Voir', 'bt_view');
-	
+
 	_import_to_session();
-	
+
 	_show_tab_session($PDOdb);
-	
+
 	$formCore->end();
-	
-	
+
+
 	dol_fiche_end();
-	
+
 	llxFooter();
 }
 
 function _show_tab_session(&$PDOdb) {
 	global $langs,$db, $user;
-	
+
 	$Tab = &$_SESSION['TDataImport'];
-	
+
 	$save = GETPOST('bt_save') ? true : false;
 	//var_dump($Tab);
 	if (!empty($Tab))
 	{
+
+	    $nb_not_here = 0;
 		foreach($Tab as $fk_product=>$TNomenclature) {
 
-			$p=new Product($db);	
-			if($p->fetch(0,$fk_product)<=0) continue;
+			$p=new Product($db);
+			if($p->fetch(0,$fk_product)<=0) {
+			    $nb_not_here++;
+			    continue;
+			}
 
 			echo '<hr />'.$p->getNomUrl(1).' - '.$p->label;
 
@@ -84,7 +91,7 @@ function _show_tab_session(&$PDOdb) {
 						$n->TNomenclatureWorkstation[$k]->rang = $k+1;
 						$n->TNomenclatureWorkstation[$k]->workstation = $w;
 					} else {
-						$p_compo=new Product($db);	
+						$p_compo=new Product($db);
 						if($p_compo->fetch(0,$data['fk_product_composant'])<=0) continue;
 
 						$k = $n->addChild($PDOdb, 'TNomenclatureDet');
@@ -103,8 +110,11 @@ function _show_tab_session(&$PDOdb) {
 			}
 
 		}
+
+		echo '<p>'.$nb_not_here.' nomenclature(s) non importée(s) car produit(s) non présent(s)</p>';
+
 	}
-	
+
 	if(!$save) {
 		$formCore=new TFormCore;
 		echo '<div class="tabsAction">';
@@ -113,87 +123,87 @@ function _show_tab_session(&$PDOdb) {
 	}
 	else {
 		print 'Nomenclatures créées';
-		
+
 	}
 }
 
 function _show_nomenclature(&$n) {
-	
+
 	global $langs,$db, $user;
-	
+
 	echo '<br />Pour : '.$n->qty_reference;
-	
+
 	if($n->getId()>0) echo '<br />Id nomenclature créée : '.$n->getId();
-	
+
 	echo '<table class="border" width="100%"><tr class="liste_titre"><td>Type</td><td>Composant</td><td>Qté</td></tr>';
-	
+
 	// Components
 	foreach($n->TNomenclatureDet as &$det) {
-		
+
 		echo '<tr>
 			<td width="10%">'.$det->code_type.'</td>
 			<td width="70%">'.$det->product->getNomUrl(1).' - '.$det->product->label.'</td>
 			<td  width="20%" align="right">'.price($det->qty).'</td>
 		</tr>';
-		
+
 	}
-	
+
 	// Workstations
 	foreach($n->TNomenclatureWorkstation as &$wst) {
-		
+
 		echo '<tr>
 			<td width="10%">'.$wst->workstation->code.'</td>
 			<td width="70%">'.$wst->workstation->getNomUrl(1).'</td>
 			<td  width="20%" align="right">'.$wst->nb_hour_manufacture.'</td>
 		</tr>';
-		
+
 	}
-	
-	
+
+
 	echo '</table>';
-	
+
 }
 
 function _import_to_session() {
-	
+
 	if(GETPOST('bt_view') && !empty($_FILES['file1']['name'])) {
 		$Tab = &$_SESSION['TDataImport'];
 		$Tab = array();
 
 		$f1 = fopen($_FILES['file1']['tmp_name'],'r');
-		
+
 		if($f1 === false) exit('Houston ? ');
-		
+
 		while(!feof($f1)) {
-			
+
 			$row = fgetcsv($f1, 4096, ',', '"');
-			
+
 			$num_nomenclature = (int)$row[0];
 			if(empty($num_nomenclature)) $num_nomenclature = 1;
-			
+
 			$fk_product = trim($row[1]);
 			if(empty($fk_product)) continue;
-			
+
 			$fk_product_composant = $row[2]; // produit ou code WS
 			if(empty($fk_product_composant)) continue;
-			
+
 			$qty = (double)$row[3];
 			$qty_ref = (double)$row[4];
 			$type = $row[5];
-			
+
 			if(empty($Tab[$fk_product]))$Tab[$fk_product]=array();
 			if(empty($Tab[$fk_product][$num_nomenclature]))$Tab[$fk_product][$num_nomenclature]=array();
-			
+
 			$Tab[$fk_product][$num_nomenclature][]=array(
 				'fk_product_composant'=>$fk_product_composant
 				,'qty'=>$qty
 				,'qty_ref'=>$qty_ref
 				,'type'=>$type
 			);
-			
+
 		}
-		
+
 		fclose($f1);
 	}
-	
+
 }
