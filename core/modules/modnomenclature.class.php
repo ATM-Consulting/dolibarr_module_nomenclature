60c9b448f01201323f506bac39db3af0160e85db
John BOTELLA <john.botella@atm-consulting.fr>
2022-08-30 17:41:09 +0200

    Fix icon

--- core/modules/modnomenclature.class.php
+++ core/modules/modnomenclature.class.php
@@ -62,7 +62,7 @@ class modnomenclature extends DolibarrModules
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 
 
-		$this->version = '4.9.0';
+		$this->version = '4.9.1';
 
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
@@ -76,7 +76,7 @@ class modnomenclature extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto='nomenclature@nomenclature';
+		$this->picto='nomenclature.svg@nomenclature';
 
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /nomenclature/core/xxxxx) (0=disable, 1=enable)
